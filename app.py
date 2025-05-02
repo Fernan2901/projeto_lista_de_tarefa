671976e67eccf7deeca147510e9894a79cb91a6a
Fernan2901 <fernado.filgueiraffr@gmail.com>
2025-05-01 22:04:04 -0300

    Update lista

--- app.py
+++ app.py
@@ -1,17 +1,71 @@
+def adicionar_tarefa(lista_de_tarefas, tarefa):
+    #adiciona
+    lista_de_tarefas.append(tarefa)
+    print(f"{'*' * 15}Tarefa adicionada com sucesso.")
+    return lista_de_tarefas
 
-import random
-random_number = random.randint(1,100)
-answer = int(input("Diga um número de 1 a 100: "))
-if answer == random_number:
-    print("Você acertou na primeira tentativa!")
-else:
-    while answer != random_number:
-        if answer > random_number:
-            print("Muito alto")
+def listar_tarefas(lista_de_tarefas):
+    #exibe
+    n = 1
+    print("\n")
+    print("-" * 50)
+    print(f"{' ' * 15}Lista de Tarefas")
+    print("-" * 50)
+    for tarefa in lista_de_tarefas:
+            print(f"{n} - {tarefa}")
+            n += 1
+
+def deletar_tarefa(lista_de_tarefas, tarefa):
+    #deleta
+    lista_de_tarefas.pop(int(tarefa -1))
+    return lista_de_tarefas
+
+def exibir_menu():
+     #exibe
+     print("-" * 50)
+     print("Escolha uma opção:\n"\
+                "1 - inserir nova tarefa\n" \
+                "2 - Listar tarefas\n" \
+                "3 - Deletar tarefa\n" \
+                "4 - Sair") 
+     print("-" * 50)
+
+#inicialização
+lista_de_tarefas = list()
+continuar = True
+
+#cabeçalho
+print("-" * 50)
+print("Bem-vindo(a) à sua Lista de Tarefas.")
+print("-" * 50)
+
+#loop principal
+while continuar :
+    exibir_menu()
+    opcao = input ("insira o que deseja fazer: ")
+
+    if opcao == "1":
+        tarefa = input('insira uma nova tarefa: ')
+        lista_de_tarefas = adicionar_tarefa(lista_de_tarefas, tarefa)
+
+    elif opcao == "2":
+       listar_tarefas(lista_de_tarefas)
+    
+    elif opcao == "3":
+        #validação
+        tarefa = input("Insira o número da tarefa que deseja deletar: ")
+        if not tarefa.isnumeric():
+            print("Inválido. Tente novamente.")
+        elif int(tarefa) > len(lista_de_tarefas):
+            print("Número inválido. Tente novamente.")
+        elif int(tarefa) <= 0:
+            print("Número inválido! Tente novamente.")
         else:
-          print("Muito baixo")
-        answer = int(input("Tente novamente: "))
+            tarefa = int(tarefa)
+        deletar_tarefa(lista_de_tarefas, int(tarefa))
+    elif opcao =="4":
+        continuar = False
     else:
-        print("você acertou!")
-
-
+        print("Opção inválida. Tente novamente.")
+    print('\n')
+    
